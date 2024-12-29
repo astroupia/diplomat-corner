e72b74a7b20c18e191c0dae4cba3e03115f08a95
zeamanuel fetene <zeamanuel1994@gmail.com>
2024-12-29 11:31:09 +0300

    Add Kinde Auth API route for authentication handling

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,23 @@
+    import React, { ReactNode } from "react";
+    import { cn } from "@/lib/utils";
+
+    const MaxWidthWrapper = ({
+    className,
+    children,
+    }: {
+    className?: string;
+    children: ReactNode;
+    }) => {
+    return (
+        <div
+        className={cn(
+            'w-full h-full',
+            className
+        )}
+        >
+        {children}
+        </div>
+    );
+    };
+
+    export default MaxWidthWrapper;
