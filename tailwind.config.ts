9ab0825ab85e6819abdeecb83d711db5204c20cd
zeamanuel fetene <zeamanuel1994@gmail.com>
2025-01-03 15:15:01 +0300

    Update primary color in Tailwind configuration. Changed primary color from CSS variable to a specific hex value for consistency in styling.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,7 +25,7 @@ const config = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#5B8F2D",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
