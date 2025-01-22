3f5620099c53abf35a551eddc10eb2a571271d4c
zeamanuel fetene <zeamanuel1994@gmail.com>
2025-01-22 11:34:27 +0300

    Add About Us section and Contact Form to Home page for enhanced user engagement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { image } from "framer-motion/client";
 import { ArrowBigDown, ArrowBigDownDash, ArrowBigDownDashIcon, ArrowDown, ChevronDown, Plus, Star } from "lucide-react";
 import Link from "next/link";
 import React from 'react';
+import CardAbout from "@/components/CardAbout";
+import ContactForm from '../components/ContactForm';
+
 
 export default function Home() {
   const images = ["/air.jpg", "/air2.jpg", "/b.jpg", "/b1.jpg","/car.jpg","wom.jpg","half.jpg"];
@@ -200,6 +203,47 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
       </section>
+
+      {/*Text Section About Us*/}
+      <section className="bg-slate-100">
+        <MaxWidthWrapper>
+          <div className="flex flex-col items-center justify-center p-6 sm:p-12">
+            <h2 className="text-4xl sm:text-5xl font-semibold mb-6 text-center text-primary" >
+              WHO WE ARE
+            </h2>
+            <p className="text-gray-700 leading-relaxed text-lg sm:text-xl px-4 sm:px-14 text-center">
+            Diplomat Corner has been serving the diplomatic community in Ethiopia since 2015, providing a wide range of services, including property management, 
+            house hunting, disposal of duty-free vehicles, car rental services, used household items sales, liaison services, online shopping, and more. Our commitment 
+            to value creation and excellent customer service has grown our subscriber base to over 3,200, consisting of embassies, UN agencies, AU organs, international organizations, 
+            and multinational companies.
+              </p>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      <section>
+        <MaxWidthWrapper>
+          <div>
+            <CardAbout/>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      <section>
+        <MaxWidthWrapper>
+          <div>
+            <ContactForm/>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    
+
+
+
+
+
+
+
     </div>
   );
 }
