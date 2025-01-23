8e27ddc948e43cf1b6b1676f0ec67925701d2139
zeamanuel fetene <zeamanuel1994@gmail.com>
2025-01-23 07:13:30 +0300

    Remove hardcoded authentication prop from NavBar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <NavBar  isAuthenticated={true} />
+        <NavBar  />
         <SignedOut>
             <SignUpButton />
           </SignedOut>
