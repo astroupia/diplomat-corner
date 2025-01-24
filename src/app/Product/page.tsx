0afe285438ce3e966237f753e940345e372bb73e
zeamanuel fetene <zeamanuel1994@gmail.com>
2025-01-24 07:20:39 +0300

    Remove ManageProductsAndAds component from Product page

--- src/app/Product/page.tsx
+++ src/app/Product/page.tsx
@@ -1,10 +1,8 @@
 import ProductForm from "@/components/product";
-import ManageProductsAndAds from "@/components/CarProduct";
 
 export default function Home() {
     return (
         <div>
-            <ManageProductsAndAds/>
         <ProductForm />
         </div>
     );
