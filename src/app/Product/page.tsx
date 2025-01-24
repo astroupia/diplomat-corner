5f455b2d021a3e1a49b8a1d94cdc6799841d513f
zeamanuel fetene <zeamanuel1994@gmail.com>
2025-01-24 06:32:16 +0300

    Add ManageProductsAndAds component to Product page for enhanced management

--- src/app/Product/page.tsx
+++ src/app/Product/page.tsx
@@ -1,8 +1,10 @@
 import ProductForm from "@/components/product";
+import ManageProductsAndAds from "@/components/CarProduct";
 
 export default function Home() {
     return (
         <div>
+            <ManageProductsAndAds/>
         <ProductForm />
         </div>
     );
