a6aa0facc57c9470bdfbcd3e00dd74bae04df2a1
zeamanuel fetene <zeamanuel1994@gmail.com>
2025-01-25 10:28:48 +0300

    refactor: update text colors in product form for consistency

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -38,7 +38,7 @@ const ProductForm = () => {
       <div className="flex flex-col lg:flex-row bg-secondary h-auto lg:h-screen bg-primary-light p-4 lg:p-6">
         {/* Sidebar */}
         <aside className="w-full lg:w-1/5 bg-secondary rounded-3xl shadow-md p-4 border-2 border-primary  lg:mb-0 pr-7 mr-4 ">  
-          <ul className="space-y-4 text-green-800 font-semibold text-sm md:text-base">
+          <ul className="space-y-4 text-primary font-semibold text-sm md:text-base">
             <li className="flex flex-row"><ShoppingCart size={20}/>Products</li>
             <li className="pl-4 flex flex-row text-primary"><Plus/> Add Products</li>
             <li className="pl-4 text-primary flex flex-row"><Pen/> Edit Products</li>
@@ -76,10 +76,10 @@ const ProductForm = () => {
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 {/* Bedroom Input */}
                 <div>
-                  <label className="block text-sm font-semibold text-green-800">Bedroom</label>
+                  <label className="block text-sm font-semibold text-primary">Bedroom</label>
                   <input
                     type="number"
-                    className="w-full p-2 border-b-2 border-green-800 focus:outline-none focus:border-primary bg-secondary"
+                    className="w-full p-2 border-b-2 border-primary focus:outline-none focus:border-primary bg-secondary"
                     placeholder="5"
                   />
                 </div>
@@ -90,11 +90,11 @@ const ProductForm = () => {
                     <Home size={36} />
                     <span className="text-sm font-semibold">House</span>
                   </button>
-                  <button className="flex flex-col items-center space-y-2 bg-secondary text-primary px-4 py-2 rounded-lg shadow-md border border-green-800">
+                  <button className="flex flex-col items-center space-y-2 bg-secondary text-primary px-4 py-2 rounded-lg shadow-md border border-primary">
                     <Home size={36} />
                     <span className="text-sm font-semibold">Apartment</span>
                   </button>
-                  <button className="flex flex-col items-center space-y-2 bg-secondary text-primary px-4 py-2 rounded-lg shadow-md border border-green-800">
+                  <button className="flex flex-col items-center space-y-2 bg-secondary text-primary px-4 py-2 rounded-lg shadow-md border border-primary">
                     <PlayCircle size={36} />
                     <span className="text-sm font-semibold">Guest House</span>
                   </button>
@@ -104,18 +104,18 @@ const ProductForm = () => {
               {/* Size and Bathroom Inputs */}
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div>
-                  <label className="block text-sm font-semibold text-green-800">Size</label>
+                  <label className="block text-sm font-semibold text-primary">Size</label>
                   <input
                     type="text"
-                    className="w-full p-2 border-b-2 border-green-800 focus:outline-none focus:border-primary bg-secondary"
+                    className="w-full p-2 border-b-2 border-primary focus:outline-none focus:border-primary bg-secondary"
                     placeholder="225 sq m"
                   />
                 </div>
                 <div>
-                  <label className="block text-sm font-semibold text-green-800">Bathroom</label>
+                  <label className="block text-sm font-semibold text-primary">Bathroom</label>
                   <input
                     type="number"
-                    className="w-full p-2 border-b-2 border-green-800 focus:outline-none focus:border-primary bg-secondary"
+                    className="w-full p-2 border-b-2 border-primary focus:outline-none focus:border-primary bg-secondary"
                     placeholder="3"
                   />
                 </div>
@@ -152,24 +152,24 @@ const ProductForm = () => {
               {/* Image Upload */}
               <div className="h-40 flex flex-col items-center justify-center border-dashed border-2 border-primary rounded-lg">
                 <Upload size={40} className="text-primary" />
-                <p className="mt-4 text-sm text-green-800">Upload media for the campaign</p>
+                <p className="mt-4 text-sm text-primary">Upload media for the campaign</p>
               </div>
   
               {/* Condition and Maintenance */}
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div>
-                  <label className="block text-sm font-semibold text-green-800">Condition</label>
+                  <label className="block text-sm font-semibold text-primary">Condition</label>
                   <input
                     type="text"
-                    className="w-full border-b-2 border-primary focus:outline-none focus:border-green-800 p-1"
+                    className="w-full border-b-2 border-primary focus:outline-none focus:border-primary p-1"
                     placeholder="Excellent"
                   />
                 </div>
                 <div>
-                  <label className="block text-sm font-semibold text-green-800">Maintenance</label>
+                  <label className="block text-sm font-semibold text-primary">Maintenance</label>
                   <input
                     type="text"
-                    className="w-full border-b-2 border-primary focus:outline-none focus:border-green-800 p-1"
+                    className="w-full border-b-2 border-primary focus:outline-none focus:border-primary p-1"
                     placeholder="Frequent"
                   />
                 </div>
@@ -177,7 +177,7 @@ const ProductForm = () => {
   
               {/* Currency and Payment Terms */}
               <div>
-                <p className="text-sm font-semibold text-green-800 mb-4">Currency</p>
+                <p className="text-sm font-semibold text-primary mb-4">Currency</p>
                 <div className="flex space-x-4">
                   {["ETB", "USD"].map((currencyOption) => (
                     <label
@@ -205,7 +205,7 @@ const ProductForm = () => {
   
               {/* Payment Terms */}
               <div className="mt-4">
-                <p className="text-sm font-semibold text-green-800 mb-2">Payment Terms</p>
+                <p className="text-sm font-semibold text-primary mb-2">Payment Terms</p>
                 <div className="flex flex-wrap gap-4">
                   {["Monthly", "Quarter", "Annual"].map((term) => (
                     <label
