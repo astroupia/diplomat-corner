1d6251b8fe2a0961b67c59ffbe76e0465b3ee6dc
zeamanuel fetene <zeamanuel1994@gmail.com>
2025-02-26 09:55:46 +0300

    refactor: update import paths for NavBar and Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/NavBar";
-import Footer from "@/components/Footer";
+import NavBar from "@/components/nav-bar";
+import Footer from "@/components/footer";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
