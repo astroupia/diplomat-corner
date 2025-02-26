8be27b8ff0809fb0c516d6ba322b138dbc826f24
zeamanuel fetene <zeamanuel1994@gmail.com>
2025-02-26 09:55:14 +0300

    feature: api handler for car module

---
+++ src/app/api/cars/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+import dbConnect from '@/lib/dbConnect';
+import Car from '@/models/car';
+export async function GET() {
+  try {
+    await dbConnect(); // Ensure DB connection
+
+    const cars = await Car.find({}); // Fetch all cars from MongoDB
+    return NextResponse.json(cars, { status: 200 });
+  } catch (error) {
+    console.error(" Error fetching cars:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
