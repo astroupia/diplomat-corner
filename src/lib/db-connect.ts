17f241faaa4a2f9b7ad73bfaa9840611ba59e7f4
astroupia <103548612+astroupia@users.noreply.github.com>
2025-02-26 12:01:00 +0300

    fixr: lint warning fixed

--- src/lib/db-connect.ts
+++ src/lib/db-connect.ts
@@ -1,14 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 console.log("MONGODB_URI:", MONGODB_URI); // Debugging line
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env');
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env"
+  );
 }
 
-let cached = (global as any).mongooseCache || { conn: null, promise: null };
+const cached = (global as any).mongooseCache || { conn: null, promise: null };
 
 async function dbConnect() {
   if (cached.conn) {
@@ -16,15 +18,18 @@ async function dbConnect() {
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      dbName: 'diplomatcorner',
-    }).then((mongoose) => {
-      console.log(' MongoDB connected successfully!');
-      return mongoose;
-    }).catch((err) => {
-      console.error(' MongoDB connection error:', err);
-      throw err;
-    });
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "diplomat-corner",
+      })
+      .then((mongoose) => {
+        console.log(" MongoDB connected successfully!");
+        return mongoose;
+      })
+      .catch((err) => {
+        console.error(" MongoDB connection error:", err);
+        throw err;
+      });
   }
 
   cached.conn = await cached.promise;
