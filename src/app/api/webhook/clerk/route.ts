7c0b1a30fc5ff601f2d4e82b40c89ebe5843736d
astroupia <103548612+astroupia@users.noreply.github.com>
2025-02-26 13:04:56 +0300

    refactor: connectToDatabase import upadated

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -2,7 +2,7 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, updateUser } from "@/lib/actions/user.actions";
-import connectToDatabase from "@/lib/db-connect";
+import { connectToDatabase } from "@/lib/db-connect";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
