dcb9385ecceda44f5adb84f21afa41ac78e57c3a
zeamanuel fetene <zeamanuel1994@gmail.com>
2025-04-09 01:00:15 +0300

    fix: update import and component usage in Home page

--- src/app/manage-product/house/page.tsx
+++ src/app/manage-product/house/page.tsx
@@ -1,9 +1,9 @@
-import ManageHouse from "@/components/manage-house";
+import ManageProduct from "@/components/manage-product";
 
 export default function Home() {
   return (
     <div>
-      <ManageHouse />
+      <ManageProduct />
     </div>
   );
 }
