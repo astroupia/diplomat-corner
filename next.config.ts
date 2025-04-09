392e18973b5e9f1276dd1a048d840787de6804cb
astroupia <103548612+astroupia@users.noreply.github.com>
2025-04-09 10:27:40 +0300

    DC-11: chore: clean up

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,33 @@
-const nextConfig = {
+import webpack from "webpack";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Disable image optimization
+    unoptimized: false,
+  },
+
+  webpack(config, { isServer }) {
+    // Custom rule for image imports (excluding public folder images)
+    config.module?.rules?.push({
+      test: /\.(png|jpe?g|gif|svg|webp|bmp|ico)$/i,
+      issuer: /\.[jt]sx?$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/images/[name].[hash][ext]",
+      },
+      exclude: /public\/assets\/images/,
+    });
+
+    // Ignore problematic re-export file from image folder (optional)
+    config.plugins?.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /.*\/public\/assets\/images\/index\.ts/,
+      })
+    );
+
+    return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
