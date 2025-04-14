bc7294adedaea902a38d6feb2c120b84f5aa7c37
astroupia <103548612+astroupia@users.noreply.github.com>
2025-04-14 10:33:08 +0300

    DC-17: content: making product array

--- public/assets/data/products.ts
+++ public/assets/data/products.ts
@@ -3,6 +3,7 @@ export interface Product {
   tag: string;
   rating: number;
   reviews: number;
+  price: number;
 }
 
 // Export products as objects with proper typing
@@ -11,31 +12,37 @@ export const products = {
     tag: "House For Rent",
     rating: 4.95,
     reviews: 22,
+    price: 145789,
   },
   car1: {
     tag: "Car For Sale",
     rating: 4.25,
     reviews: 22,
+    price: 145668,
   },
   house2: {
     tag: "House For Rent",
     rating: 3.95,
     reviews: 22,
+    price: 120000,
   },
   car2: {
     tag: "Car For Sale",
     rating: 4.6,
     reviews: 22,
+    price: 13000,
   },
   car3: {
     tag: "Car For Sale",
     rating: 4.3,
     reviews: 22,
+    price: 8900,
   },
   placeholder: {
-    tag: "Unknown",
+    tag: "Home",
     rating: 0,
     reviews: 0,
+    price: 99900,
   },
 };
 
